f6d2ee59fd40d248d06cb47b4c9b7a7a36457c26
Zulnorain <muhammadzulnorain2@gmail.com>
2024-12-06 23:31:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const connectToDb = require('./db/db')
+
+connectToDb();
+
+
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.send('home page at 3000');
+})
+
+
+module.exports = app
